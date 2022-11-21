5df415d57ba8cd6e71837d941d7168168daa4b7d
maxwellenemuo <maxwell.enemuo@gmail.com>
2022-11-21 14:30:05 +0100

    first commit

---
+++ package/README.md
@@ -0,0 +1,4 @@
+# color-npm-package
+
+Test NPM package:
+https://www.npmjs.com/package/@max_biaggi/get-color
---
+++ package/index.js
@@ -0,0 +1,5 @@
+function getColor(color) {
+  return color === "green" ? "#79ff75" : "#000000";
+}
+
+module.exports = getColor;
